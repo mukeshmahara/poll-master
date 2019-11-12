0ec846e44564f26a198721c1b0a75abc07c58bc1
CodeDestroyer12 <sajan12dhakal@gmail.com>
2019-11-12 12:26:56 +0545

    merging master

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,24 @@
 const express = require ("express");
+const mongoose = require("mongoose")
+const cors = require("cors"); //Cors is an expres middle ware that enable cors, cross origin
+
 const app = express();
-app.listen(5000,()=>{
-    console.log("Listening at pot 5000");
+
+const port =process.env.PORT || 5000;
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true });
+const connection = mongoose.connection;
+
+connection.once('open', ()=>{
+    console.log("mongooDb database connection established successfully!");  
 })
+
+app.listen(port,()=>{
+    console.log(`Listening at port: ${port}`);
+});
