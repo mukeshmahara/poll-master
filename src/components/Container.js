bf6b85bc0d47a68b53c4f12b3753f6a22bdca35c
CodeDestroyer12 <sajan12dhakal@gmail.com>
2019-11-20 12:19:00 +0545

    poll components added

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import PollContainer from './PollContainer'
 
 
 function Container() {
+    let optionArr = ["React", "Node", "Vue", "angular"];
+	let animeArr = ["One Piece", "Naruto"];
+
     return (
         <div>
 
@@ -14,38 +17,15 @@ function Container() {
             <hr />
             <div className="container-fluid">
                 <div className="row">
-                <div className="col-12 col-sm-6 col-md-4  containerBox mb-2 d-flex justify-content-center align-item-stretch">
-                        <div className="border text-center p-2 rounded" style={{width: "20rem", height: "15rem", backgroundColor: "#E9ECEF"}}>
-                        Here is Question and Answer Components
-
-                            </div>
-                        {/* <Question />
-                        <Option /> */}
-                    </div>
-                    <div className="col-12 col-sm-6 col-md-4  containerBox mb-2 d-flex justify-content-center align-item-stretch">
-                        <div className="border text-center p-2 rounded" style={{width: "20rem", height: "15rem", backgroundColor: "#E9ECEF"}}>
-                        Here is Question and Answer Components
-
-                            </div>
-                        {/* <Question />
-                        <Option /> */}
-                    </div>
-                    <div className="col-12 col-sm-6 col-md-4  containerBox mb-2 d-flex justify-content-center align-item-stretch">
-                        <div className="border text-center p-2 rounded" style={{width: "20rem", height: "15rem", backgroundColor: "#E9ECEF"}}>
-                        Here is Question and Answer Components
-
-                            </div>
-                        {/* <Question />
-                        <Option /> */}
-                    </div>
-                    <div className="col-12 col-sm-6 col-md-4  containerBox mb-2 d-flex justify-content-center align-item-stretch">
-                        <div className="border text-center p-2 rounded" style={{width: "20rem", height: "15rem", backgroundColor: "#E9ECEF"}}>
-                        Here is Question and Answer Components
-
-                            </div>
-                        {/* <Question />
-                        <Option /> */}
-                    </div>
+                    <PollContainer options={optionArr} question="Fav Js" />
+                    <PollContainer options={animeArr} question="Fav Anime" />
+                    {/* <PollContainer options={optionArr} question="Fav Js" />
+                    <PollContainer options={optionArr} question="Fav Js" />
+                    <PollContainer options={optionArr} question="Fav Js" />
+                    <PollContainer options={optionArr} question="Fav Js" />*/}
+                    <PollContainer options={optionArr} question="Fav Js" /> 
+                    
+                    
                 </div>
             </div>
         </div >
---
+++ src/components/Question.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Question(props) { 
+    return (
+        <p>
+            {props.question}
+        </p>
+    );
+}
+
+export default Question;
