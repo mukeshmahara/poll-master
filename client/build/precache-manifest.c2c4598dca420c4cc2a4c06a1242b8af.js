b3d400519dc7ff1f8ec2f4db56d5e79e98b14969
Sushil <bajracharya.sushil01@gmail.com>
2019-11-30 11:42:59 +0545

    removed build from gitignore

---
+++ client/build/precache-manifest.c2c4598dca420c4cc2a4c06a1242b8af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494c809fb07e530858477927ea5188ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a1f37a91638729b9a6",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "23f93dbf42d5581e093c",
+    "url": "/static/css/main.3e52d2f1.chunk.css"
+  },
+  {
+    "revision": "76a1f37a91638729b9a6",
+    "url": "/static/js/2.8d8c2f22.chunk.js"
+  },
+  {
+    "revision": "23f93dbf42d5581e093c",
+    "url": "/static/js/main.1bbf420c.chunk.js"
+  },
+  {
+    "revision": "a9ecda04f1d758ce37e3",
+    "url": "/static/js/runtime-main.9a4055b6.js"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/static/media/404.6695af99.svg"
+  },
+  {
+    "revision": "8da3f613838e2ede1ab3c4df4c369be3",
+    "url": "/static/media/PageNotFound.8da3f613.svg"
+  },
+  {
+    "revision": "a69fcf6e8f215bfbc461a2b2cba39223",
+    "url": "/static/media/loading.a69fcf6e.gif"
+  },
+  {
+    "revision": "a3a70abaf6edc85ae2560417fec02674",
+    "url": "/static/media/pollmaster-logo.a3a70aba.svg"
+  }
+]);
