28dac819e54b33c2df203b9ed920893877b28921
Sushil <bajracharya.sushil01@gmail.com>
2019-11-30 12:14:47 +0545

    /poll route added to production

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ connection.once('open', ()=>{
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     
+    app.use('/poll', PollRouter);
+    
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
