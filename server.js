fd91dc4ad4f4301ce7ca8e9606e7c7632b609c82
Sushil <bajracharya.sushil01@gmail.com>
2019-11-30 12:01:40 +0545

    routes managed and backendserverjs deleted

--- server.js
+++ server.js
@@ -26,16 +26,16 @@ connection.once('open', ()=>{
     console.log("mongooDb database connection established successfully!");  
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './index.html'));
+// });
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../build'));
+    app.use(express.static('client/build'));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
