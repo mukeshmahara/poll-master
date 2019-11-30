afe356c581fad798f0f589a725f8cbdca697cbfa
Sushil <bajracharya.sushil01@gmail.com>
2019-11-30 11:21:34 +0545

    folder structure changed

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ connection.once('open', ()=>{
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
+    app.use(express.static('../build'));
     
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, './index.html'));
